8d778dba981944a2a0dfeeb618ff4564c09a77af
Troy Hart <thart@myriad.com>
2022-01-29 17:59:51 -0700

    attempting to add an integration test, but it did not work out as advertised.

---
+++ src/test/java/com/mgl/releaseverification/HelloWorldMainIT.java
@@ -0,0 +1,10 @@
+package com.mgl.releaseverification;
+
+import io.quarkus.test.junit.main.QuarkusMainIntegrationTest;
+
+// TODO: fix me! according to https://quarkus.io/guides/command-mode-reference, I should have been able to simply
+//  annotate this class with @QuarkusMainIntegrationTest and it should have worked....but either I don't understand
+//  or the quarkus guide is wrong/has a bug.
+//@QuarkusMainIntegrationTest
+public class HelloWorldMainIT extends HelloWorldMainTest {
+}
