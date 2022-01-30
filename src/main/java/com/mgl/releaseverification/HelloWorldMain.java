f6dfb8735dae8cc1b6ba957b2a84d6a18bcd4fce
Troy Hart <thart@myriad.com>
2022-01-29 17:50:03 -0700

    Added some tests, with QuarkusMainTest

--- src/main/java/com/mgl/releaseverification/HelloWorldMain.java
+++ src/main/java/com/mgl/releaseverification/HelloWorldMain.java
@@ -10,6 +10,8 @@ import java.util.stream.Stream;
 public class HelloWorldMain implements QuarkusApplication {
     @Override
     public int run(String... args) throws Exception {
+        if (args==null || args.length==0) return 1;
+
         System.out.println(Stream.of(args).map(String::toUpperCase).collect(Collectors.joining("], [Hello ", "[Hello ", "]")));
         return 0;
     }
--- src/test/java/com/mgl/releaseverification/HelloWorldMainTest.java
+++ src/test/java/com/mgl/releaseverification/HelloWorldMainTest.java
@@ -1,7 +1,36 @@
 package com.mgl.releaseverification;
 
-import static org.junit.jupiter.api.Assertions.*;
+import io.quarkus.test.junit.main.Launch;
+import io.quarkus.test.junit.main.LaunchResult;
+import io.quarkus.test.junit.main.QuarkusMainLauncher;
+import io.quarkus.test.junit.main.QuarkusMainTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+@QuarkusMainTest
 class HelloWorldMainTest {
 
+    @Test
+    @Launch("World")
+    public void testLaunchCommand(LaunchResult result) {
+        Assertions.assertEquals("[Hello WORLD]", result.getOutput());
+    }
+
+    @Test
+    @Launch({"world", "bOy"})
+    public void testLaunchCommandWithMultipleArgs(LaunchResult result) {
+        Assertions.assertEquals("[Hello WORLD], [Hello BOY]", result.getOutput());
+    }
+
+    @Test
+    @Launch(value = {}, exitCode = 1)
+    public void testLaunchCommandFailed() {
+    }
+
+    @Test
+    public void testManualLaunch(QuarkusMainLauncher launcher) {
+        LaunchResult result = launcher.launch("Everyone");
+        Assertions.assertEquals(0, result.exitCode());
+        Assertions.assertEquals("[Hello EVERYONE]", result.getOutput());
+    }
 }
